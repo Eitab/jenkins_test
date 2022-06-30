fe9a0eab97b66f10bcdfdc4208c6c784f8292fa2
“[eitab <[valid-email]>
2022-06-30 13:23:39 +0300

    modify python file

--- test_newbalance_website_grid.py
+++ test_newbalance_website_grid.py
@@ -1,42 +1,41 @@
-import time
-import warnings
-from threading import Thread
 import pytest
+
 from selenium import webdriver
-from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.firefox.service import Service as firefoxService
-import pytest_check as check  # install pytest_check package
-
 
 @pytest.fixture()
 def driver():
-    firefox_driver_binary = "./geckodriver.exe"
-    ser_firefox = firefoxService(firefox_driver_binary)
+    firefox_driver_binary = "./drivers/geckodriver"
+    ser_firefox = FirefoxService(firefox_driver_binary)
     firefox_options = FireFoxOptions()
 
+    brave_path = "/usr/bin/brave-browser"
+    options = webdriver.ChromeOptions()
+    options.binary_location = brave_path
+
     browser_name = 'firefox'
-    if browser_name == "firefox":
+    # if isinstance(browserName,list):
+    #     for browser_name in browserName:
+    if browser_name == "firefox-webdriver":
         driver = webdriver.Firefox(service=ser_firefox)
     elif browser_name == "firefox":
         dc = {
             "browserName": "firefox",
+            # "browserVersion": "101.0.1(x64)",
             "platformName": "Windows 11"
         }
         driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc, options=firefox_options)
 
-    elif browser_name == "MicrosoftEdge":
+    elif browser_name == "brave":
         dc = {
-            "browserName": "MicrosoftEdge",
+            "browserName": "chrome",
             "platformName": "Windows 11"
         }
-        driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc)
+        driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc, options=options)
 
     elif browser_name == "chrome":
         dc = {
@@ -44,17 +43,41 @@ def driver():
             "platformName": "Windows 11"
         }
         driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc)
+
     elif browser_name == "firefox-mobile":
         firefox_options = FireFoxOptions()
         firefox_options.add_argument("--width=375")
         firefox_options.add_argument("--height=812")
         firefox_options.set_preference("general.useragent.override", "userAgent=Mozilla/5.0 "
-                                                                     "(iPhone; CPU iPhone OS 15_4 like Mac OS X) "
-                                                                     "AppleWebKit/605.1.15 (KHTML, like "
+                                                                     "(iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like "
                                                                      "Gecko) CriOS/101.0.4951.44 Mobile/15E148 Safari/604.1")
         # firefox_options.set_preference("general.useragent.override", "Nexus 7")
 
         driver = webdriver.Firefox(service=ser_firefox, options=firefox_options)
+
+    elif browser_name == "android-emulator":
+        dc = {
+            "platformName": "Android",
+            "platformVersion": "8.1.0",
+            "deviceName": "Android Emulator",
+            # "platformVersion": "11.0.0",
+            # "deviceName": "1aaa4ea80404",
+            "automationName": "Appium",
+            # "app": "com.android.chrome",
+            "browserName": "Chrome"
+        }
+        driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
+
+    elif browser_name == "android-phone":
+        dc = {
+            "platformName": "Android",
+            "platformVersion": "11.0.0",
+            "deviceName": "1aaa4ea80404",
+            "automationName": "Appium",
+            "browserName": "Chrome"
+        }
+
+        driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
     else:
         raise Exception("driver doesn't exists")
     yield driver
