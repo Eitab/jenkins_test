fd77aba959d07f1cfb2bef9e24fc879e9a20cf63
“[eitab <[valid-email]>
2022-07-06 12:14:33 +0300

    test status code and text stub

--- test.py
+++ test.py
@@ -8,3 +8,12 @@ def test_base_route_hello_world():
 def test_text():
     response = requests.get("http://localhost:5000/")
     assert "Welcome to the Application with updated code!" == response.text
+
+
+def test_base_route_stub():
+    response = requests.get("http://localhost:5000/stub")
+    assert response.status_code == 200
+
+def test_text_stub():
+    response = requests.get("http://localhost:5000/stub")
+    assert "Value of Stub: 200" == response.text
